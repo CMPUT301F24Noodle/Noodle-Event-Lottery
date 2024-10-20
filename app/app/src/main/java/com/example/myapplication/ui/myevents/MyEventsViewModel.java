bef39660ade34ca734e6f7874fc6cf65b9cc931c
weolins <weolinsyt@gmail.com>
2024-10-20 01:38:54 -0600

    More Refactoring

--- app/app/src/main/java/com/example/myapplication/ui/myevents/MyEventsViewModel.java
+++ app/app/src/main/java/com/example/myapplication/ui/myevents/MyEventsViewModel.java
@@ -1,14 +1,14 @@
-package com.example.myapplication.ui.gallery;
+package com.example.myapplication.ui.myevents;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class GalleryViewModel extends ViewModel {
+public class MyEventsViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public GalleryViewModel() {
+    public MyEventsViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is gallery fragment");
     }
