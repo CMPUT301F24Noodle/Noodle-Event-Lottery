7ead54cffe0e503bbb87ec3fd82277bedd92c2a9
weolins <weolinsyt@gmail.com>
2024-10-24 16:42:00 -0600

    Added Registered Event View

--- app/app/src/main/java/com/example/myapplication/ui/registeredevents/RegisteredEventFragment.java
+++ app/app/src/main/java/com/example/myapplication/ui/registeredevents/RegisteredEventFragment.java
@@ -4,12 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.myapplication.databinding.FragmentRegisteredEventsBinding;
+import com.example.myapplication.ui.home.CustomAdapter;
+import com.example.myapplication.ui.home.ListItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RegisteredEventFragment extends Fragment {
 
@@ -23,6 +29,16 @@ public class RegisteredEventFragment extends Fragment {
             binding = FragmentRegisteredEventsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        ListView listView = binding.registeredEventList;
+
+        List<ListItem> sampleList = new ArrayList<>();
+        sampleList.add(new ListItem("Event Alpha", "12 March 2023", "Event Time", "Organizer's Name"));
+        sampleList.add(new ListItem("Event Beta", "12 April 2022", "Why are we here?", "Sleepy Rn"));
+
+        // Set up the custom adapter
+        CustomAdapter adapter = new CustomAdapter(requireContext(), sampleList);
+
+        listView.setAdapter(adapter);
         return root;
     }
 
