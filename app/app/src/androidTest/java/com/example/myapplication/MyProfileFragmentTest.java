32f9b6f6a2e89db3ea3a171223321cb1ce795d99
xaviersalm <justxaviersalm@gmail.com>
2024-11-07 00:52:19 -0700

    Added two UI tests for MyProfileFragment

--- app/app/src/androidTest/java/com/example/myapplication/MyProfileFragmentTest.java
+++ app/app/src/androidTest/java/com/example/myapplication/MyProfileFragmentTest.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
@@ -63,12 +64,40 @@ public class MyProfileFragmentTest {
     // Test if the user's details are correctly displayed
     @Test
     public void DisplayUserInfoTest(){
+
         // navigate to my profile
         onView(withContentDescription("Open navigation drawer")).perform(click());
         onView(withId(R.id.nav_profile)).perform(click());
         onView(withId(R.id.profile_facility_section_text)).check(matches(isDisplayed()));
 
         // check if text matches
+        onView(withId(R.id.profile_user_name)).check(matches(withText("Name")));
+        onView(withId(R.id.profile_user_email)).check(matches(withText("Email")));
+    }
+
+    // Test if the save button works
+    @Test
+    public void SaveUserInfoButtonTest(){
+
+        // navigate to my profile
+        onView(withContentDescription("Open navigation drawer")).perform(click());
+        onView(withId(R.id.nav_profile)).perform(click());
+        onView(withId(R.id.profile_facility_section_text)).check(matches(isDisplayed()));
+
+        // type in a number, and then give empty string for user name
+        onView(withId(R.id.profile_user_contact_number)).perform(ViewActions.typeText("123456789"));
+        onView(withId(R.id.profile_user_name)).perform(replaceText(""));
+
+        // press save
+        onView(withId(R.id.profile_save_info_button)).perform(click());
+
+        // check if the expected values are in the text fields
+        onView(withId(R.id.profile_user_name)).check(matches(withText("Name")));
+        onView(withId(R.id.profile_user_contact_number)).check(matches(withText("123456789")));
+    }
+
+    @Test
+    public void FacilityVisibilityToggleTest(){
 
     }
 
