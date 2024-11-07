ea8f21c5db16d97602877d458bfb56b7cb16850b
xaviersalm <justxaviersalm@gmail.com>
2024-11-06 18:49:34 -0700

    Changed default name and email for users

--- app/app/src/main/java/com/example/myapplication/objects/userProfileClasses/UserProfile.java
+++ app/app/src/main/java/com/example/myapplication/objects/userProfileClasses/UserProfile.java
@@ -55,8 +55,8 @@ public class UserProfile implements Serializable {
      */
     public UserProfile(String uuid) {
 
-        this.name = "None";
-        this.email = "None";
+        this.name = "Name";
+        this.email = "Email";
         this.privileges = 0; //defaults to entrant privileges
         this.allowNotifs = Boolean.TRUE; //defaults to allow notifications
         this.geoLocationOn = Boolean.FALSE; //defaults to false, need to ask user for permission first
